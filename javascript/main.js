1e3433fb36f26993fd221aa6bf4c2ca4f0f98c1a
katamatata <51786805+katamatata@users.noreply.github.com>
2020-03-31 18:34:42 +0200

    fixing bugs, refactoring

--- javascript/main.js
+++ javascript/main.js
@@ -1,38 +1,38 @@
-let apiKey = "90b4bf234e22493e89325b6f9cce868e";
-let numberOfResults = 10;
+const apiKey = "90b4bf234e22493e89325b6f9cce868e";
+const numberOfResults = 10;
 
-let randomRecipesUrl = "https://api.spoonacular.com/recipes/random?apiKey=" + apiKey;
+const randomRecipesEndpoint = "https://api.spoonacular.com/recipes/random?apiKey=" + apiKey;
 
-let recipesByIngredientsUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey;
+const recipesByIngredientsEndpoint = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey;
 
-let recipeFullInfoEndpointUrl = "https://api.spoonacular.com/recipes/";
+const recipeFullInfoEndpoint = "https://api.spoonacular.com/recipes/";
 
 let favouriteRecipesArray = [];
 
-function getFavouriteRecipesArray() {
-    let favouriteRecipes = localStorage.getItem('favouriteRecipes');
+const getFavouriteRecipesArray = () => {
+    const favouriteRecipes = localStorage.getItem('favouriteRecipes');
     if (favouriteRecipes !== null) {
         favouriteRecipesArray = JSON.parse(favouriteRecipes);
     }
 }
 
-function saveFavouriteRecipes() {
+const saveFavouriteRecipes = () => {
     localStorage.setItem('favouriteRecipes', JSON.stringify(favouriteRecipesArray));
 }
 
-function fillFavouriteRecipesDropdown() {
+const fillFavouriteRecipesDropdown = () => {
     getFavouriteRecipesArray();
-    console.log(favouriteRecipesArray);
+    //console.log(favouriteRecipesArray);
+    document.getElementById("dropdownInner").innerHTML = '';
     if (favouriteRecipesArray.length > 0) {
-        document.getElementById("dropdownInner").innerHTML = '';
         for (let recipe of favouriteRecipesArray) {
-            let recipeEl = document.createElement('div');
+            const recipeEl = document.createElement('div');
             recipeEl.className = 'recipeListEl';
             document.getElementById("dropdownInner").appendChild(recipeEl);
             fillListItem(recipeEl, recipe);
         }
     } else {
-        let favouritesPlaceholder = document.createElement('div');
+        const favouritesPlaceholder = document.createElement('div');
         favouritesPlaceholder.className = 'favouritesPlaceholder';
         document.getElementById('dropdownInner').appendChild(favouritesPlaceholder);
         favouritesPlaceholder.innerHTML = "No favourite recipes yet";
@@ -40,74 +40,72 @@ function fillFavouriteRecipesDropdown() {
     }
 }
 
-function showFavouriteRecipes() {
+const showFavouriteRecipes = () => {
     document.getElementById('dropdown').style.display = 'block';
 }
 
-function hideFavouriteRecipes() {
+const hideFavouriteRecipes = () => {
     document.getElementById('dropdown').style.display = 'none';
 }
 
-function getNumberPerSearch() {
+const getNumberPerSearch = () => {
     return document.getElementById("resultsPerSearch").value;
 }
 
-async function onSearchClicked() {
-    let userInput = document.getElementById("searchField").value;
+const onSearchClicked = async () => {
+    const userInput = document.getElementById("searchField").value;
     if (userInput === '') {
         return null;
     }
-    let results = await searchRecipesByIngredients(userInput);
+    const results = await searchRecipesByIngredients(userInput);
     showResults(results);
 }
 
-async function getRandomRecepies() {
-    let url = randomRecipesUrl + "&number=" + numberOfResults;
-    let response = await fetch(url);
-    let recipeList = await response.json();
-    return recipeList.recipes;
+const getRandomRecepies = async () => {
+    const randomRecipesUrl = randomRecipesEndpoint + "&number=" + numberOfResults;
+    const responseRandomRecipes = await fetch(randomRecipesUrl);
+    const recipeListRandom = await responseRandomRecipes.json();
+    return recipeListRandom.recipes;
 }
 
-async function showRandomRecipes() {
-    let results = await getRandomRecepies();
+const showRandomRecipes = async () => {
+    const results = await getRandomRecepies();
     showResults(results);
 }
 
-async function searchRecipesByIngredients(items) {
-    let url = recipesByIngredientsUrl + "&number=" + numberOfResults + "&ingredients=" + encodeURIComponent(items);
-    // console.log(url);
-    let response = await fetch(url);
-    let recipeList = await response.json();
-    // console.log(recipeList);
-    return recipeList;
+const searchRecipesByIngredients = async (items) => {
+    const recipesByIngredientsUrl = recipesByIngredientsEndpoint + "&number=" + numberOfResults + "&ingredients=" + encodeURIComponent(items);
+    const responseRecipesByIngredients = await fetch(recipesByIngredientsUrl);
+    const recipeListByIngredients = await responseRecipesByIngredients.json();
+    return recipeListByIngredients;
 }
 
-function fillListItem(element, recipe) {
-    let thumbnailImg = document.createElement('img');
+const fillListItem = (element, recipe) => {
+    const thumbnailImg = document.createElement('img');
     thumbnailImg.className = 'thumb';
     thumbnailImg.src = recipe.image;
     element.appendChild(thumbnailImg);
 
-    let recipeInfoEl = document.createElement('div');
+    const recipeInfoEl = document.createElement('div');
     recipeInfoEl.className = 'recipeListInfo';
     element.appendChild(recipeInfoEl);
 
-    let recipeName = document.createElement('h2');
+    const recipeName = document.createElement('h2');
     recipeName.className = 'recipeListName';
     recipeName.textContent = recipe.title;
     recipeInfoEl.appendChild(recipeName);
 }
 
-function showResults(recipeList) {
+const showResults = (recipeList) => {
     clearResults();
     clearRecipe();
 
     console.log(recipeList);
 
-    let allElements = [];
+    const allElements = [];
 
     for (let i = 0; i < recipeList.length; i++) {
-        let recipeEl = document.createElement('div');
+        const recipeEl = document.createElement('div');
         recipeEl.className = 'recipeListEl';
 
         allElements.push(recipeEl);
@@ -122,57 +120,57 @@ function showResults(recipeList) {
         }
 
         document.getElementById("recipeListSection").appendChild(recipeEl);
-        fillListItem(recipeEl, recipeList[i])
+        fillListItem(recipeEl, recipeList[i]);
     }
     allElements[0].classList.add('selected');
     showRecipe(recipeList[0]);
 
 }
 
-function clearResults() {
-    let clearResults = document.getElementById("recipeListSection");
+const clearResults = () => {
+    const clearResults = document.getElementById("recipeListSection");
     clearResults.innerHTML = "";
 }
 
-async function getRecipeFullInfo(item) {
-    let url = recipeFullInfoEndpointUrl + item + "/information?includeNutrition=true&apiKey=" + apiKey;
-    let response = await fetch(url);
-    let recipeInfo = await response.json();
+const getRecipeFullInfo = async (item) => {
+    const recipeFullInfoUrl = recipeFullInfoEndpoint + item + "/information?includeNutrition=true&apiKey=" + apiKey;
+    const responserecipeFullInfo = await fetch(recipeFullInfoUrl);
+    const recipeInfo = await responserecipeFullInfo.json();
     return recipeInfo;
 }
 
-async function showRecipe(recipe) {
+const showRecipe = async (recipe) => {
     clearRecipe();
 
-    let recipeFullInfo = await getRecipeFullInfo(recipe.id);
+    const recipeFullInfo = await getRecipeFullInfo(recipe.id);
 
-    let bigImgWrapper = document.createElement('div');
+    const bigImgWrapper = document.createElement('div');
     bigImgWrapper.className = 'img-wrapper';
     bigImgWrapper.style.backgroundImage = 'url(' + recipeFullInfo.image + ')';
     document.getElementById("recipeSection").appendChild(bigImgWrapper);
 
-    let recipeName = document.createElement('h2');
+    const recipeName = document.createElement('h2');
     recipeName.className = 'recipeName';
     recipeName.textContent = recipeFullInfo.title;
     bigImgWrapper.appendChild(recipeName);
 
-    let recipeInfoEl = document.createElement('div');
+    const recipeInfoEl = document.createElement('div');
     recipeInfoEl.className = 'recipeInfo';
     document.getElementById("recipeSection").appendChild(recipeInfoEl);
 
-    let iconsWrapper = document.createElement('div');
+    const iconsWrapper = document.createElement('div');
     iconsWrapper.className = 'iconsWrapper';
     recipeInfoEl.appendChild(iconsWrapper);
 
-    let servingsEl = document.createElement('div');
+    const servingsEl = document.createElement('div');
     servingsEl.className = 'servingsEl';
     iconsWrapper.appendChild(servingsEl);
 
-    let utensilsIcon = document.createElement('i');
+    const utensilsIcon = document.createElement('i');
     utensilsIcon.className = 'fas fa-utensils';
     servingsEl.appendChild(utensilsIcon);
 
-    let servings = document.createElement('span');
+    const servings = document.createElement('span');
     servings.className = 'servings';
     if (recipeFullInfo.servings === 1) {
         servings.textContent = recipeFullInfo.servings + ' serving';
@@ -181,15 +179,15 @@ async function showRecipe(recipe) {
     }
     servingsEl.appendChild(servings);
 
-    let cookingTimeEl = document.createElement('div');
+    const cookingTimeEl = document.createElement('div');
     cookingTimeEl.className = 'cookingTimeEl';
     iconsWrapper.appendChild(cookingTimeEl);
 
-    let clockIcon = document.createElement('i');
+    const clockIcon = document.createElement('i');
     clockIcon.className = 'far fa-clock';
     cookingTimeEl.appendChild(clockIcon);
 
-    let cookingTime = document.createElement('span');
+    const cookingTime = document.createElement('span');
     cookingTime.className = 'cookingTime';
     cookingTime.textContent = recipeFullInfo.readyInMinutes + ' min';
     cookingTimeEl.appendChild(cookingTime);
@@ -202,11 +200,11 @@ async function showRecipe(recipe) {
         }
     }
 
-    let heartEl = document.createElement('div');
+    const heartEl = document.createElement('div');
     heartEl.className = 'heartEl';
     iconsWrapper.appendChild(heartEl);
-    let heartIcon = document.createElement('i');
-    let addToFavourites = document.createElement('span');
+    const heartIcon = document.createElement('i');
+    const addToFavourites = document.createElement('span');
 
     if (isRecipeFavourite) {
         heartIcon.className = 'fas fa-heart';
@@ -231,48 +229,47 @@ async function showRecipe(recipe) {
     heartEl.appendChild(addToFavourites);
 
 
-    let infoWrapper = document.createElement('div');
+    const infoWrapper = document.createElement('div');
     infoWrapper.className = 'infoWrapper';
     recipeInfoEl.appendChild(infoWrapper);
 
-    let ingredientsWrapper = document.createElement('div');
+    const ingredientsWrapper = document.createElement('div');
     ingredientsWrapper.className = 'ingredientsWrapper';
     infoWrapper.appendChild(ingredientsWrapper);
 
-    let ingredientsWrapperName = document.createElement('p');
+    const ingredientsWrapperName = document.createElement('p');
     ingredientsWrapperName.className = 'ingredientsWrapperName';
     ingredientsWrapperName.textContent = 'Ingredients:';
     ingredientsWrapper.appendChild(ingredientsWrapperName);
 
-    let ingredientsEl = document.createElement('ul');
+    const ingredientsEl = document.createElement('ul');
     ingredientsEl.className = 'ingredientsEl';
     ingredientsWrapper.appendChild(ingredientsEl);
 
     for (let i = 0; i < recipeFullInfo.extendedIngredients.length; i++) {
-        let ingredient = document.createElement('li');
+        const ingredient = document.createElement('li');
         ingredient.textContent = recipeFullInfo.extendedIngredients[i].original;
         ingredientsEl.appendChild(ingredient);
     }
 
-    let instructionsWrapper = document.createElement('div');
+    const instructionsWrapper = document.createElement('div');
     instructionsWrapper.className = 'instructionsWrapper';
     infoWrapper.appendChild(instructionsWrapper);
 
-    let instructionsWrapperName = document.createElement('p');
+    const instructionsWrapperName = document.createElement('p');
     instructionsWrapperName.className = 'instructionsWrapperName';
     instructionsWrapperName.textContent = 'Preparation:';
     instructionsWrapper.appendChild(instructionsWrapperName);
 
-    let instructionsEl = document.createElement('p');
+    const instructionsEl = document.createElement('p');
     instructionsEl.className = 'instructionsEl';
     instructionsEl.textContent = recipeFullInfo.instructions;
     instructionsWrapper.appendChild(instructionsEl);
 }
 
-function clearRecipe() {
-    let clearResults = document.getElementById("recipeSection");
+const clearRecipe = () => {
+    const clearResults = document.getElementById("recipeSection");
     clearResults.innerHTML = "";
-    // console.log(clearResults);
 }
 
 document.getElementById('heart').onclick = function () {
